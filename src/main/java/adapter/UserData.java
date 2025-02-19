b193a32458e0261216aacdd5a249d3e4d33ec233
SERENA FERRARO <serenaferraro21@gmail.com>
2025-02-19 13:59:22 +0000

    adapter

---
+++ src/main/java/adapter/UserData.java
@@ -0,0 +1,33 @@
+package adapter;
+
+public class UserData {
+    private String nomeCompleto;
+    private  int eta;
+
+
+    public  void getData(DataSource ds){
+        nomeCompleto = ds.getNomeCompleto();
+        eta = ds.getEta();
+    }
+
+    public UserData(String nomeCompleto, int eta) {
+        this.nomeCompleto = nomeCompleto;
+        this.eta = eta;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+}
